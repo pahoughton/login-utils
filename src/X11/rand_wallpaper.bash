7e4a257081ffd8ec1a6ada6cbed2122d4a3b45ec
Paul Houghton <paul4hough@gmail.com>
2014-03-21 14:04:39 -0600

    opps logic was reversed

--- src/X11/rand_wallpaper.bash
+++ src/X11/rand_wallpaper.bash
@@ -13,14 +13,14 @@ while true; do
   for pic in $backgrounds; do
     echo $pic > ~/.wallpaper.pic
     if grep '/background/' ~/.wallpaper.pic > /dev/null ; then
-      display  -backdrop -background '#3f3f3f' -flatten  \
-	-resize 2560x1440^ -window root  -gravity Center \
-	$pic
-    else
       display  -backdrop -background '#3f3f3f' -flatten -window root   \
 	-gravity Center -resize 2560x1440^ -crop 2560x1440+0+0 +repage \
 	-resize 2560x1440^                                             \
-	$pic
+	"${pic}"
+    else
+      display  -backdrop -background '#3f3f3f' -flatten  \
+	-resize 2560x1440^ -window root  -gravity Center \
+	"${pic}"
     fi
     sleep $freq &
     echo $! > ~/.wallpaper-sleep.pid
