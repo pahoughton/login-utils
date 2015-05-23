9d82d7bcacef3193ea8016dd1ca26276b505508d
Paul Houghton <paul4hough@gmail.com>
2015-05-23 12:50:50 -0700

    new perl wallpaper script

--- src/X11/next_wallpaper.bash
+++ src/X11/next_wallpaper.bash
@@ -1,6 +1,3 @@
 #!/bin/bash
-## next_wallpaper.bash - 2014-03-01 12:31
-#
-# Copyright (c) 2014 Paul Houghton <paul4hough@gmail.com>
-#
-kill `cat ~/.wallpaper-sleep.pid`
+# 2014-03-01 (cc) paul4hough@gmail.com
+kill -SIGALRM `cat ~/.pid.wallpaper`
